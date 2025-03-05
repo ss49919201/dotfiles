db287ca468721b22a2cccd5ec04bd001b7898f10
Shinya Sakae <ss49919201@gmail.com>
2025-03-06 07:43:07 +0900

    Update git script

--- script/git.sh
+++ script/git.sh
@@ -9,3 +9,4 @@ git config --global delta.side-by-side true
 git config --global delta.line-numbers true
 git config --global merge.conflictstyle "diff3"
 git config --global diff.colorMoved "default"
+git config --global init.defaultBranch main
