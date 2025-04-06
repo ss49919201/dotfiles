d1aff866f18753a706f168ea664f3f6a97e3a6b8
Shinya Sakae <ss49919201@gmail.com>
2025-04-06 23:36:10 +0900

    update wezterm config

--- _wezterm.lua
+++ _wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require 'wezterm'
-
+local act = wezterm.action
 local config = {}
+
 if wezterm.config_builder then
     config = wezterm.config_builder()
 end
@@ -12,5 +13,13 @@ config.window_background_opacity = 0.8
 -- Font
 config.font = wezterm.font("UDEV Gothic", {weight="Medium", stretch="Normal", style="Normal"})  
 config.font_size = 14
-  
+
+config.keys = {
+	-- Swtich tab
+	-- Same as in Chrome
+    -- mac
+    {key="LeftArrow", mods="CMD|OPT", action=wezterm.action{ActivateTabRelative=-1}},
+    {key="RightArrow", mods="CMD|OPT", action=wezterm.action{ActivateTabRelative=1}},
+}
+
 return config
